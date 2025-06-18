949677109fbe08bd188ece4a526d6b061c1429eb
Yann Vignolet <yann.vignolet@api-and-you.com>
2025-06-18 15:16:41 +0200

    ajout des traductions manquantes en francais

--- I18n/backOffice/default/fr_FR.php
+++ I18n/backOffice/default/fr_FR.php
@@ -1,6 +1,24 @@
 <?php
 
-return array(
-    'Chronopost Tax Rule configuration' => 'Configuration des Règles de taxes Chronopost',
-    'Tax Rule' => 'Règles de taxes',
-);
+return [
+    'Chronopost Tax Rule configuration'    => 'Configuration des Règles de taxes Chronopost',
+    'Tax Rule'                             => 'Règles de taxes',
+    'Weight up to ... kg'                  => 'Poids jusqu\'à ... kg',
+    'Untaxed Price up to ... %symbol'      => 'Prix hors taxe jusqu\'à ... %symbol',
+    'Price (%symbol)'                      => 'Prix (%symbol)',
+    'Actions'                              => 'Actions',
+    'Activate free shipping from (€) :'    => 'Activer la livraison gratuite à partir de (€) :',
+    'Area : '                              => 'Zone : ',
+    'Or activate free shipping from (€) :' => 'Ou activer la livraison gratuite à partir de (€) :',
+    'Activate total free shipping '        => 'Activer la gratuité totale des frais de port',
+    'Price slices for \"%mode\"'           => 'Tranches de prix pour \"%mode\"',
+    'Advanced configuration'               => 'Configuration avancée',
+    'Delete this price slice'              => 'Supprimer cette tranche de prix',
+    'Save this price slice'                => 'Enregister cette tranche de prix',
+    'Message'                              => 'Message',
+    'manage shipping zones'                => 'gérer les zones d\'expédition',
+    'Add this price slice'                 => 'Ajouter cette tranche de prix',
+    'Allowed shipping modes'               => 'Modes d\'expédition autorisés',
+    'You should first attribute shipping zones to the modules: '
+                                           => 'Vous devez d\'abord attribuer des zones d\'expédition aux modules :',
+];
